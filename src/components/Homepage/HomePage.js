84841d5d993dd846748df4140f6e73eb99f5874d
dastageer <dastageer@zendyanmix.com>
2023-07-26 22:05:33 +0530

    temp stash

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -4,25 +4,15 @@ function HomePage() {
     <div className={style.main_container}>
       <div className={style.outer_border}>
         <div className={style.card_container}>
+
           <div className={style.bg_image}>
-            <img src={require('../../assets/images/headshot.jpg')} alt='profile pic'></img>
+              <img src={require('../../assets/images/headshot.jpg')} alt='profile pic'></img>
+            <div className={style.glass_overlay}>
+            </div>
           </div>
           <div className={style.content_container}>
-            <div>
-              test lorem
-              test loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest lorem
-              test loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest loremtest lorem
+            <div className={style.dp_container}>
+              <img src={require('../../assets/images/headshot.jpg')} alt='profile pic'></img>
 
             </div>
           </div>
